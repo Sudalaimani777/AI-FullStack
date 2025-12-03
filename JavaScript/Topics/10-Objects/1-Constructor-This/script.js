17695167a5fcc9e4046d789b178ac3cb7217f09d
Sudalaimani777 <sudalaimani5112007@gmail.com>
2025-12-03 09:19:48 +0530

    Add built-in constructor examples in script.js and create corresponding index.html for structure

--- JavaScript/Topics/10-Objects/1-Constructor-This/script.js
+++ JavaScript/Topics/10-Objects/1-Constructor-This/script.js
@@ -1,11 +1,11 @@
 //Constructor Rule :-
 /**
- * 1) The first letter should be capital
- * 2) If we want to create the constructor into the object, we need to use the "new" as a keyword infront of the constructor name to assign it in the another variable
+ * 1) The first letter should be capital.
+ * 2) If we want to create the constructor into the object, we need to use the "new" as a keyword infront of the constructor name to assign it in the another variable.
+ * 3) The constructor creates the instances of the object.
  */
 
 //ES5 :-
-
 //Created the constructor (Function)
 function UserName(fName, lName, address) {
     console.log("This is the constructor function");
@@ -39,3 +39,56 @@ console.log(this);
 // this.alert("123");
 
 //this === window
+
+//Normal Function inside the object :-
+function normal () {
+    console.log("This is the normal function but we called 'this'  keyword ->", this);
+}
+normal();
+
+//"this" in the object :-
+const thisInObject = {
+    a : "A Key and a value (Return the parent (Own object itself)",
+    b: function(){
+        console.log(this);
+        return this.a;
+    }
+}
+console.log(thisInObject.b());
+
+const singleThis = {
+    k : function(){
+        return "This returns the own object ->", this;
+    }
+}
+console.log(singleThis);
+
+//Neated Method inside the object :-
+const nestedMethod = {
+    //key -> Method
+    key: function(){
+        console.log("key ->", this); //Returns the object nestedMethod
+    //But inside the 'key' method we have the seperate function :-
+    function seperateFunction(){
+        console.log("Nested function inside the method -> ", this); //Returns the window object because it is an normnal function
+    }
+    seperateFunction();
+    }
+}
+nestedMethod.key();
+
+// nestedMethod.key() -> this === nestedMethod
+// inside key() -> seperateFunction === Standalone call, so here "this" returns the "window object";
+
+//To fix the issue we can use the "Lexical this" :-
+const fixTheIssue = {
+    lexical: function(){
+        console.log("Inside the normal method which returns the object itself -> ", this);
+        // function lexicalThis (){} // Returns the window object, so we can use the arrow function to access the object itsel using "this";
+        const lexicalThis = () =>{
+            console.log("The arrow functio returs the onject if it was placed inside the method -> ", this);
+        }
+        lexicalThis()
+    }
+}
+fixTheIssue.lexical();
