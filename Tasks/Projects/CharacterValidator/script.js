f7442e0e5c297a540289af7ba2597e60e733e764
Sudalaimani777 <sudalaimani5112007@gmail.com>
2025-12-03 10:23:16 +0530

    Add initial implementation of Character Validator with form validation logic and update tasks list

---
+++ Tasks/Projects/CharacterValidator/script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#form");
+const checkBtn = document.querySelector("#btn");
+const para = document.querySelector("#para");
+
+function loadAllEvent (){
+    //Form Event :-
+    form.addEventListener("keyup", inputData);
+    //Submit Btn :-
+    checkBtn.addEventListener("submit", checkValidData);
+}
+loadAllEvent();
+
+function inputData (e){
+    let value = e.target.value.toLowerCase();
+    if(value.length < 6){
+        checkBtn.disabled = true;
+        para.textContent = "Text must be at least 6 characters long.";
+    }
+    else{
+        checkBtn.disabled = false;
+        para.textContent = "Correct Input.";
+    }
+}
+
+function checkValidData(e){
+    e.preventDefault();
+}
