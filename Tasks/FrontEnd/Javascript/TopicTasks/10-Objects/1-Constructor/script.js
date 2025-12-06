86988a0034eb3dae49f1cfc1f2e068f012177ddc
Sudalaimani777 <sudalaimani5112007@gmail.com>
2025-12-06 17:51:48 +0530

    Refactor BankAccount methods for consistency and add nested method examples to demonstrate lexical 'this'

--- Tasks/FrontEnd/Javascript/TopicTasks/10-Objects/1-Constructor/script.js
+++ Tasks/FrontEnd/Javascript/TopicTasks/10-Objects/1-Constructor/script.js
@@ -41,43 +41,43 @@
 // userDetails.getFullName();
 
 //Task 4: Implement methods inside constructor functions using 'this'
-function BankAccount (accHolder, balanceAmount) {
+function BankAccount(accHolder, balanceAmount) {
     this.accountHolder = accHolder;
     this.bankBalance = balanceAmount;
 
     //Deposite Amount :-
-    this.depositeAmount = function(amount){
-        if(amount > 0){
+    this.depositeAmount = function (amount) {
+        if (amount > 0) {
             this.bankBalance += amount;
             return `Deposited Money -> $${amount}, Total Balance $${this.bankBalance}`;
         }
-        else{
-            retun `Invalid amount`
+        else {
+            retun`Invalid amount`
         }
     }
     //Check Bank Balance :-
-    this.checkBalance = function(){
+    this.checkBalance = function () {
         return `The account holder name is ${this.accountHolder} having the balance amount of ${this.bankBalance}`
     }
 
     //WithDraw Amount :-
-    this.withdrewAmount = function(amount){
-        if(amount > 0 && amount <= this.bankBalance){
+    this.withdrewAmount = function (amount) {
+        if (amount > 0 && amount <= this.bankBalance) {
             this.bankBalance -= amount;
             return `The withdrawal amount is $${amount} the remaining balance is $${this.bankBalance}`
         }
-        else{
+        else {
             return `Something went wrong please check the amount and details`
         }
     }
     //Transfer Money to another bank account :-
-    this.transaction = function(amount, accHolder){
-        if(amount > 0 && amount <= this.bankBalance){
+    this.transaction = function (amount, accHolder) {
+        if (amount > 0 && amount <= this.bankBalance) {
             this.bankBalance -= amount;
             accHolder.bankBalance += amount;
             return `The transferredd amonunt is $${amount} to the coustomer MR/MS ${accHolder.accountHolder}`
         }
-        else{
+        else {
             return `Transaction Failed`;
         }
     }
@@ -100,3 +100,29 @@ console.log(maniAccount.transaction(20, raviAccount));
 console.log(raviAccount.checkBalance())
 
 console.log(maniAccount.checkBalance());
+
+//Task 5: Create nested methods and understand lexical 'this' with arrow functions
+
+const understandLexical = {
+    obj: function () {
+        console.log("This -> ", this);
+        function insideMethod() {
+            console.log("This is the inside method and it returns the global this  ->", this);
+        }
+        insideMethod();
+    }
+}
+understandLexical.obj();
+
+//To Avoid these kinda issue we can use the lexical this :-
+const toAvoidTheGlobalThisInsideTheMethod = {
+    obj: function () {
+        console.log("this ->", this);
+        //Lexical This (ES6 Arrow Functio) :-
+        const avLexical = () => {
+            console.log("Lexical this to catch the parent (method) ->" , this)
+        };
+        avLexical();
+    }
+}
+toAvoidTheGlobalThisInsideTheMethod.obj();
