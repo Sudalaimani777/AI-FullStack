15f83b07700bade90490d51a88a7d51330be8367
Sudalaimani777 <sudalaimani5112007@gmail.com>
2026-01-05 12:54:23 +0530

    Add IIFE and modular pattern implementation: create iifeNotes.txt, module.js, and index.html for modular pattern demonstration, enhance script.js with data and UI controllers

--- FrontEnd/JavaScript/Topics/15-ModularPattern/2-ImmediatelyInvokeFunctionExpression/module.js
+++ FrontEnd/JavaScript/Topics/15-ModularPattern/2-ImmediatelyInvokeFunctionExpression/module.js
@@ -1,3 +1,50 @@
+//Normal Function :-
+const a = function(){
+    console.log("Normal Function");
+}
+
+
+//IIFE (Immediately Invoked Function Expression) :-
+const iife = (() => {
+    console.log(`This is the IIFE`)
+})();
+
+const greet ="This is the greet which is in the outer of the IIFE";
+console.log("Outer Greet -> ",greet)
+
+const iifeFunction = (() => {
+    //Private :-
+    const x= 10;
+    const greet= "This is the greet inside the modular iife"
+
+    //We need to return the Private codes to access it in the outer
+    return {
+        x: x,
+        userGreet: function(){
+            return greet;
+        }
+    } 
+})(); //Calling the function using the parenthesis "()"
+
+console.log(iifeFunction.x);
+console.log(iifeFunction.userGreet());
+
+const addTwoNums = ((function add(a, b) {
+    const result = a + b;
+    console.log(result);
+})(2, 4));
+
+//Syntax :-
+(function funcName(param1, param2){
+
+}());
+
+//ES6 :-
+(() => {
+    let a= 10;
+
+})();
+
 //App Controller, Data Contorller, UI Controller :-
 
 //Data Controller :-
