82f511a8ff3350869403b33d231e317f6449fc98
Sudalaimani <sudalaimani5112007@gmail.com>
2026-02-16 21:21:24 +0530

    fix: Update script source in index.html and refine Thought prototype summary method in thought.js

--- Projects/FrontEnd/JavascriptProjects/Mind/thought.js
+++ Projects/FrontEnd/JavascriptProjects/Mind/thought.js
@@ -6,12 +6,16 @@ export function Thought (content, type) {
 }
 
 // Adding the prototype
-Thought.prototype.summary = function(){
+// Thought.prototype.summary = function(){
+//     return `${this.type.toUpperCase()} : ${this.content}`
+// }
+
+
+
+Thought.prototype.summary = function ()  {
     return `${this.type.toUpperCase()} : ${this.content}`
 }
 
 
-
-
-
-
+const data = new Thought("Learn JavaScript", "task");
+console.log(data.summary());
