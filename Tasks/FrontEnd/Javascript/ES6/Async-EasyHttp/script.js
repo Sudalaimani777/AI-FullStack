fa72ca74008c6ad2a66d507c784ba2fdc884fed9
K.Sudalaimani <sudalaimani5112007@gmail.com>
2026-02-28 22:34:21 +0530

    Update script.js

--- Tasks/FrontEnd/Javascript/ES6/Async-EasyHttp/script.js
+++ Tasks/FrontEnd/Javascript/ES6/Async-EasyHttp/script.js
@@ -4,8 +4,6 @@ const userData = {
     user:"Sudalaimani"
 }
 
-
-
 const data = UserDataLibrary;
 data.get('https://jsonplaceholder.typicode.com/users').then(user => console.log(user)).catch(err => console.log(err));
 
