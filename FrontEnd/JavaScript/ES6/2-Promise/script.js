312d4ba91957d0e1fc8fc9e8e443a190a26c0a43
Sudalaimani <sudalaimani5112007@gmail.com>
2026-03-31 23:21:08 +0530

    feat: add examples of using async/await and .then() for fetching user data

--- FrontEnd/JavaScript/ES6/2-Promise/script.js
+++ FrontEnd/JavaScript/ES6/2-Promise/script.js
@@ -137,3 +137,24 @@ const myMobileStatus = new Promise((resolve, reject) => {
 })
 myMobileStatus.then(message => console.log(message)).catch(err => console.log(err)).finally(message => console.log(`Thankyou For Visiting....`))
 
+//Note: If you want to use async/await with try/catch, you can do it like this:
+async function fetchUserDataWithAsyncAwait (){
+    try{
+        const response = await fetch(API_URL); //Returns the Promise so we need to use await
+        const data = await response.json(); //Returns the Promise so we need to use await
+        console.log(data);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+fetchUserDataWithAsyncAwait();
+
+//Note: If you want to use .then() and .catch() instead of async/await, you can do it like this:
+function fetchUserDataWithThenCatch (){
+    fetch(API_URL)
+    .then(response => response.json()) //Returns the Promise so we need to use .then()
+    .then(data => console.log(data)) //Returns the Promise so we need to use .then()
+    .catch(err => console.log(err)); //Handle the error using .catch()
+}
+fetchUserDataWithThenCatch();
