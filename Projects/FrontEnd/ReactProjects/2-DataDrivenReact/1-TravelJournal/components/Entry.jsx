315c165a6c093e5ae7a795bdece053ecf1af308a
Sudalaimani <sudalaimani5112007@gmail.com>
2026-04-05 11:40:23 +0530

    refactor: simplify Entry component props by destructuring entry object

--- Projects/FrontEnd/ReactProjects/2-DataDrivenReact/1-TravelJournal/components/Entry.jsx
+++ Projects/FrontEnd/ReactProjects/2-DataDrivenReact/1-TravelJournal/components/Entry.jsx
@@ -1,11 +1,11 @@
-export default function Entry(props) {
+export default function Entry({entry}) {
     return (
         <article className="journal-entry">
             <div className="main-image-container">
                 <img 
                     className="main-image"
-                    src={props.img.src} 
-                    alt={props.img.alt}
+                    src={entry.img.src} 
+                    alt={entry.img.alt}
                 />
             </div>
             <div className="info-container">
@@ -14,11 +14,11 @@ export default function Entry(props) {
                     src="../images/marker.png" 
                     alt="map marker icon"
                 />
-                <span className="country">{props.country}</span>
-                <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="trip-dates">{props.dates}</p>
-                <p className="entry-text">{props.text}</p>
+                <span className="country">{entry.country}</span>
+                <a href={entry.googleMapsLink} target="_blank">View on Google Maps</a>
+                <h2 className="entry-title">{entry.title}</h2>
+                <p className="trip-dates">{entry.dates}</p>
+                <p className="entry-text">{entry.text}</p>
             </div>
             
         </article>
