9f7b96ffc63fcbd4012aca86652e21c3aec4fe8b
Sudalaimani <sudalaimani5112007@gmail.com>
2026-04-06 17:44:22 +0530

    refactor: update state management in App component to use callback functions

--- Projects/FrontEnd/ReactProjects/3-UsingState/1-SimpleCounterApp/App.jsx
+++ Projects/FrontEnd/ReactProjects/3-UsingState/1-SimpleCounterApp/App.jsx
@@ -4,17 +4,19 @@ export default function App() {
     const [count, setCount] = React.useState(0)
     
     /**
-     * Challenge: 
-     * Add functionality to the minus button
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
      */
     
-    function add() {
-        setCount(count + 1)
-    }
+    const add = () => setCount(prevCount => prevCount + 1);
     
-    function subtract() {
-        setCount(count - 1)
-    }    
+    const subtract = () => setCount(prevCount => prevCount - 1);  
     
     return (
         <main className="container">
