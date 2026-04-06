b9c065014958d2b39cf07dd193088f32d79bd500
Sudalaimani <sudalaimani5112007@gmail.com>
2026-04-06 17:50:45 +0530

    refactor: clean up whitespace and improve comments in App component

--- Projects/FrontEnd/ReactProjects/3-UsingState/1-SimpleCounterApp/App.jsx
+++ Projects/FrontEnd/ReactProjects/3-UsingState/1-SimpleCounterApp/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function App() {
     const [count, setCount] = React.useState(0)
-    
+
     /**
      * Note: if you ever need the old value of state
      * to help you determine the new value of state,
@@ -13,11 +13,35 @@ export default function App() {
      * which you can then use to determine your new
      * value of state.
      */
-    
+
+    //     1. You have 2 options for what you can pass in to a
+    //    state setter function (e.g. `setCount`). What are they?
+    //    1. Pass the new version of state that we want to use as the 
+    //       replacement for the old version of state.
+    //    2. Pass a callback function. Must return what we want the new
+    //       value of state to be. Will receive the old version of state
+    //       as a parameter so we can use it to help determine what we want 
+    //       the new value of state to be.
+
+
+    // 2. When would you want to pass the first option (from answer
+    //    above) to the state setter function?
+
+    //    Whenever we don't really care about (or need) the old value,
+    //    we simply want to set a new value.
+
+
+    // 3. When would you want to pass the second option (from answer
+    //    above) to the state setter function?
+
+    //    Whenever we do care about the previous value in state and need
+    //    it to help us determine what the new value should be.
+
+
     const add = () => setCount(prevCount => prevCount + 1);
-    
-    const subtract = () => setCount(prevCount => prevCount - 1);  
-    
+
+    const subtract = () => setCount(prevCount => prevCount - 1);
+
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
