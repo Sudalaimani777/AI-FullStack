e6c914b34e6e51d5b919790f223c92f4cea596ed
Sudalaimani <sudalaimani5112007@gmail.com>
2026-04-08 17:46:26 +0530

    feat: implement initial version of contact card application with state management and styling

---
+++ Tasks/FrontEnd/React/3-UseState/3-ComplexStateObject/App.jsx
@@ -0,0 +1,59 @@
+import React from "react"
+import avatar from "./images/user.png"
+import starFilled from "./images/star-filled.png"
+import starEmpty from "./images/star-empty.png"
+
+export default function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (212) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+    /**
+     * Challenge: Use a ternary to determine which star image variable
+     * should be used based on the `contact.isFavorite` property. Test 
+     * your results by manually changing the isFavorite value in state.
+     * 
+     * `true` => starFilled
+     * `false` => starEmpty
+     */
+    
+    let starIcon = contact.isFavorite ? starFilled
+
+    function toggleFavorite() {
+        console.log("Toggle Favorite")
+    }
+
+    return (
+        <main>
+            <article className="card">
+                <img
+                    src={avatar}
+                    className="avatar"
+                    alt="User profile picture of John Doe"
+                />
+                <div className="info">
+                    <button
+                        onClick={toggleFavorite}
+                        aria-pressed={false}
+                        className="favorite-button"
+                    >
+                        <img
+                            src={starEmpty}
+                            alt="empty star icon"
+                            className="favorite"
+                        />
+                    </button>
+                    <h2 className="name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="contact">{contact.phone}</p>
+                    <p className="contact">{contact.email}</p>
+                </div>
+
+            </article>
+        </main>
+    )
+}
