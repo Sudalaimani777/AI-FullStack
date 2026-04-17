e50b95b40b2ada27881a86248d33d26d52e7299a
Sudalaimani <sudalaimani5112007@gmail.com>
2026-04-17 22:52:14 +0530

    Refactor code structure for improved readability and maintainability

--- Tasks/FrontEnd/React/5-UseEffect/WindowTracker/WindowTracker.jsx
+++ Tasks/FrontEnd/React/5-UseEffect/WindowTracker/WindowTracker.jsx
@@ -3,10 +3,17 @@ import React from "react"
 export default function WindowTracker() {
     const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
     
+    //Here we are adding an event listener to the window object that listens for the "resize" event. Whenever the window is resized, the handleWindowWidth function is called, which updates the windowWidth state with the new width of the window. The useEffect hook also returns a cleanup function that removes the event listener when the component is unmounted, preventing memory leaks and ensuring that the event listener does not continue to run after the component is no longer in use.
     React.useEffect(() => {
-        window.addEventListener("resize", function() {
+        window.addEventListener("resize", handleWindowWidth)
+        function handleWindowWidth() {
+            console.log("Running")
             setWindowWidth(window.innerWidth)
-        })
+        }
+        return () => {
+            console.log("Cleaning up...")
+            window.removeEventListener('resize', handleWindowWidth)
+        }
     }, [])
     
     return (
