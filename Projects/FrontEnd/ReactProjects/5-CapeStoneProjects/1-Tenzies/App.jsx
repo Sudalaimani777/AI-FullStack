086856377c2bea526e130698eafd2e5c7a876511
Sudalaimani <sudalaimani5112007@gmail.com>
2026-04-19 19:30:50 +0530

    feat: update dice generation logic and enhance button styles

--- Projects/FrontEnd/ReactProjects/5-CapeStoneProjects/1-Tenzies/App.jsx
+++ Projects/FrontEnd/ReactProjects/5-CapeStoneProjects/1-Tenzies/App.jsx
@@ -10,18 +10,24 @@ export default function App() {
         const dieNum = [];
         for (let i = 0; i < 10; i++) {
             const randomNum = Math.ceil(Math.random() * 6);
-            dieNum.push(randomNum);
+            dieNum.push({value: randomNum, isHeld: false, id: i});
         }
         return dieNum
     }
+
+    function handleRollDice() {
+        setNumbers(prevDice => prevDice = generateNewDie());
+    }
+
 
     return (
         <main>
             <div className="dice-container">
                 {
-                    numbers.map(number => <Die number={number} />)
+                    numbers.map(number => <Die number={number.value} />)
                 }
             </div>
+                <button className="roll-dice" onClick={handleRollDice}>Roll Dice</button>
         </main>
     )
 }
