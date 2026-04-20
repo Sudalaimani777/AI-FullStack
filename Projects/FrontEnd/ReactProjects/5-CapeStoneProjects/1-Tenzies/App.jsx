b830dc97387da68babac5c7d1b53b0e23b8ed1a8
Sudalaimani <sudalaimani5112007@gmail.com>
2026-04-20 17:15:53 +0530

    feat: add confetti effect on game win and update roll button text; refactor dice roll logic

--- Projects/FrontEnd/ReactProjects/5-CapeStoneProjects/1-Tenzies/App.jsx
+++ Projects/FrontEnd/ReactProjects/5-CapeStoneProjects/1-Tenzies/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import Die from "./Die"
+import Confetti from "react-confetti"
 
 export default function App() {
 
-    const [numbers, setNumbers] = useState(generateNewDie());
-    console.log(numbers)
+    const [numbers, setNumbers] = useState(() => generateNewDie()); //We use the callback function 
+    // console.log(numbers);
+
+    const gameWon = numbers.every(die => die.isHeld) && numbers.every(die => die.value === numbers[0].value);
 
     function generateNewDie() {
         const dieNum = [];
@@ -16,17 +19,23 @@ export default function App() {
     }
 
     function handleRollDice() {
-        setNumbers(prevDice => prevDice.map(die => die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }));
+        if (!gameWon) {
+            setNumbers(prevDice => prevDice.map(die => die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }));
+        } else {
+            const newDice = generateNewDie();
+            setNumbers(newDice);
+        }
     }
 
-    const hold = id => {
-        console.log(id)
+    function hold(id) {
+        // console.log(id)
         setNumbers(prevDice => prevDice.map(die => die.id === id ? { ...die, isHeld: !die.isHeld } : die));
     }
 
 
     return (
         <main>
+            {gameWon && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
@@ -34,7 +43,7 @@ export default function App() {
                     numbers.map(number => <Die key={number.id} number={number.value} backgroundColor={number.isHeld ? "#59E391" : "white"} hold={hold} id={number.id} />)
                 }
             </div>
-            <button className="roll-dice" onClick={handleRollDice}>Roll Dice</button>
+            <button className="roll-dice" onClick={handleRollDice}>{gameWon ? "New Game" : "Roll Dice"}</button>
         </main>
     )
 }
