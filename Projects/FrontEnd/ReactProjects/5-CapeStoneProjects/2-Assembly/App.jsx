ded587b1d5d753799ea1e2204c76fe9aaf5660c4
sudalamani777 <sudalaimani5112007@gmail.com>
2026-04-25 21:41:44 +0530

    Add initial project setup with package.json and Vite configuration
    - Created package.json with dependencies for React and Vite
    - Added Vite configuration file for React plugin integration

---
+++ Projects/FrontEnd/ReactProjects/5-CapeStoneProjects/2-Assembly/App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+/**
+ * Goal: Build out the main parts of our app
+ * 
+ * Challenge: Add a header with the game title
+ * and description. Startin' out easy 🙂🚶‍➡️
+ */
+
+export default function AssemblyEndgame() {
+    return (
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the 
+                programming world safe from Assembly!</p>
+            </header>
+        </main>
+    )
+}
