1364e93a3e7cdbdb7c90dedbcd95917bea6e4eab
sudalamani777 <sudalaimani5112007@gmail.com>
2026-04-29 07:19:15 +0530

    feat: refactor search functionality and improve input handling in App component

--- Tasks/FrontEnd/React/3-UseState/5-BrandFilter/src/App.jsx
+++ Tasks/FrontEnd/React/3-UseState/5-BrandFilter/src/App.jsx
@@ -4,36 +4,29 @@ import { animeData } from './Data/anime.js'
 import Form from './Shared/Form.jsx'
 
 function App() {
-
   const [search, setSearch] = useState("");
 
   const handleSubmit = e => e.preventDefault();
-
-  const handleAnimeFiler = e => setSearch(e.target.value);
+ 
+  const handleAnimeSearch = e => setSearch(e.target.value);
 
   const filterAnime = animeData.filter(anime => anime.animeName.toLowerCase().includes(search.toLowerCase()));
- 
+
   return (
     <>
-      <section>
+     <section>
         <Form onSubmit={handleSubmit}>
-           <p>
-              <label htmlFor="searchAnime">Search Anime</label>
-              <input 
-                type="text" 
-                name="searchAnime" 
-                id="searchAnime" 
-                onChange={handleAnimeFiler}
-                value={search}
-              />
-           </p>
+            <label htmlFor="search">Search</label>
+            <input type="text" name="search" id="search" onChange={handleAnimeSearch} value={search}/>
         </Form>
-      </section>
-      <section>
-        { animeData && filterAnime.map(anime => (
-          <h3 id={anime.id}>{anime.animeName}</h3>
-        ))}
-      </section>
+     </section>
+     <section>
+        {
+          animeData && filterAnime.map(anime => (
+            <h4 key={anime.id}>{anime.animeName}</h4>
+          ))
+        }
+     </section>
     </>
   )
 }
