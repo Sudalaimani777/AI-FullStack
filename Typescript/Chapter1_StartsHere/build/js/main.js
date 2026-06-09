c68388ea4fa73d9979b1dcb31ef769827b94e050
sudalamani777 <sudalaimani5112007@gmail.com>
2026-06-09 23:00:03 +0530

    feat: update TypeScript files with explicit data types and enhance notes on TypeScript usage

--- Typescript/Chapter1_StartsHere/build/js/main.js
+++ Typescript/Chapter1_StartsHere/build/js/main.js
@@ -1,5 +1,14 @@
 const userName = "Sudalaimani Kannan";
 console.log(userName);
-const userAge = 23;
+const userAge = 22;
 console.log(userAge);
-
+// const a = 10;
+// const b = 20;
+// console.log(a / b); //Returns type error , because the typescript expects the datatype of the variable when the code executes
+// console.log(a * b);
+// To slove these kinda issue, we explicitely tells the datatypes of the particular varialble holds, the syntax will be -> 
+const firstNumber = 22;
+const secondNumber = 73;
+console.log(firstNumber / secondNumber);
+console.log(firstNumber * secondNumber);
+export {};
