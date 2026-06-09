c69dbd53d15c305be9e3a9dab396a9a3713d2afb
sudalamani777 <sudalaimani5112007@gmail.com>
2026-06-09 23:15:14 +0530

    feat: improve comments and structure in main.ts for better TypeScript understanding

--- FrontEnd/Typescript/Chapter1_StartsHere/src/main.ts
+++ FrontEnd/Typescript/Chapter1_StartsHere/src/main.ts
@@ -4,15 +4,16 @@ console.log(userName);
 const userAge = 22;
 console.log(userAge);
 
-// const a = 10;
-// const b = 20;
-// console.log(a / b); //Returns type error , because the typescript expects the datatype of the variable when the code executes
+// TypeScript can automatically infer that `a` and `b` are numbers,
+// so no type error occurs here.
 
-// console.log(a * b);
+const a = 10;
+const b = 20;
+console.log(a / b);
+console.log(a * b);
 
-// To slove these kinda issue, we explicitely tells the datatypes of the particular varialble holds, the syntax will be -> 
-
-const firstNumber : number = 22;
-const secondNumber : number = 73;
+// We can also explicitly write the type if we want:
+const firstNumber: number = 22;
+const secondNumber: number = 73;
 console.log(firstNumber / secondNumber);
 console.log(firstNumber * secondNumber);
