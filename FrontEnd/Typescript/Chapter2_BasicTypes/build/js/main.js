109c18fbd6860228e1971bcaaf2d10e33cf18ab3
sudalamani777 <sudalaimani5112007@gmail.com>
2026-06-10 22:16:06 +0530

    feat: enhance TypeScript examples and notes for basic data types

--- FrontEnd/Typescript/Chapter2_BasicTypes/build/js/main.js
+++ FrontEnd/Typescript/Chapter2_BasicTypes/build/js/main.js
@@ -1,4 +1,32 @@
-const userName = "Sudalaimani";
-console.log(userName);
-console.log("Hii");
+//Basic Data Types :-
+//String :-
+let userName = "Sudalaimani Kannan"; //Now the TS will no longer infering the variable because now it already knows the variable holds the string data type that's because we explicitely declared.
+// userName = 23 //Throws an error, we need only re-assign with the mentioned data type
+userName = "Arun";
+//Number :-
+let favouriteNumber = 7;
+console.log(favouriteNumber);
+// We can specify the data type that is present in the function paramenter :-
+const greetUser = (user) => `Welcome ${user}`;
+// console.log(greetUser(4)) //Return error
+console.log(greetUser("Sudalaimani Kannan"));
+const sum = (a, b) => a + b;
+console.log(sum(5, 2));
+//Boolean :-
+let isLogin = true;
+// Null -> (Intentionally empty value)
+let userFavoiriteFood = null;
+console.log(userFavoiriteFood);
+// Undefined :- (Value Not Assigned)
+let value = undefined;
+//any -> (accepts all kinda data types) :-
+let userFavoirite = "Cricket";
+console.log(userFavoirite);
+//unknown: Safer version of any.
+let isActive = "Yes";
+if (typeof isActive === "string")
+    console.log(isActive);
+// Union :- Allows multipe data type which is specified
+let id = "1";
+console.log(id);
 export {};
--- FrontEnd/Typescript/Chapter2_BasicTypes/src/main.ts
+++ FrontEnd/Typescript/Chapter2_BasicTypes/src/main.ts
@@ -1,3 +1,41 @@
-const userName = "Sudalaimani";
-console.log(userName);
-console.log("Hii");
+//Basic Data Types :-
+
+//String :-
+let userName: string = "Sudalaimani Kannan"; //Now the TS will no longer infering the variable because now it already knows the variable holds the string data type that's because we explicitely declared.
+
+// userName = 23 //Throws an error, we need only re-assign with the mentioned data type
+userName = "Arun";
+
+//Number :-
+let favouriteNumber: number = 7;
+console.log(favouriteNumber);
+
+// We can specify the data type that is present in the function paramenter :-
+const greetUser = (user:string) => `Welcome ${user}`
+// console.log(greetUser(4)) //Return error
+console.log(greetUser("Sudalaimani Kannan"));
+
+const sum = (a:number,b:number) => a + b;
+console.log(sum(5,2));
+
+//Boolean :-
+let isLogin: boolean = true;
+
+// Null -> (Intentionally empty value)
+let userFavoiriteFood: null = null;
+console.log(userFavoiriteFood);
+
+// Undefined :- (Value Not Assigned)
+let value: undefined = undefined;
+
+//any -> (accepts all kinda data types) :-
+let userFavoirite: any = "Cricket";
+console.log(userFavoirite);
+
+//unknown: Safer version of any.
+let isActive: unknown = "Yes";
+if(typeof isActive === "string") console.log(isActive);
+
+// Union :- Allows multipe data type which is specified
+let id: string | number = "1";
+console.log(id);
