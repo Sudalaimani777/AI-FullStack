373ac51a1db3ae6910de9ba32ef0b17efc7f3b85
sudalamani777 <sudalaimani5112007@gmail.com>
2026-06-12 22:12:48 +0530

    feat: add various function examples including optional, default, and rest parameters in TypeScript

--- FrontEnd/Typescript/Chapter4_Functions/build/js/main.js
+++ FrontEnd/Typescript/Chapter4_Functions/build/js/main.js
@@ -16,4 +16,40 @@ const greetUser = (user) => {
     console.log(user.name);
 };
 greetUser({ name: "Sudalaimani" });
+//Anonymous Function :-
+const subtract = function (c, d) {
+    return c + d;
+};
+console.log(subtract(16, 5));
+const multiplication = (a, b) => a * b;
+console.log(multiplication(2, 4));
+const division = (a, b) => a / b;
+console.log(division(6, 2));
+// In Anonymous Function
+let mul = function (a, b) {
+    return a * b;
+};
+console.log(mul(6, 2));
+const addition = (a, b) => a + b;
+console.log(addition(4, 5));
+// Anonymous Func :-
+const additionFunc = function (a, b) {
+    return a + b;
+};
+console.log(additionFunc(30, 24));
+// Optional Parmeter :- We use ? in the func params to define the params as optional and it is defined as undefined
+const addAll = (a, b, c) => {
+    if (typeof c === "undefined") {
+        return a + b;
+    }
+    return a + b + c;
+};
+// Default Params :-
+const sumAll = (a, b, c = 4) => a + b + c;
+console.log(sumAll(3, 4)); //If the default params is in the first or the other places except the last, we must pass undefined when we invoke the function
+const minusAll = (a = 3, b = 4, c) => a + b + c;
+console.log(minusAll(undefined, undefined, 10)); //Because without we use undefined the number 10 is assigned to the first or the respective params.
+// Rest Params (Operator):-
+const restParams = (...nums) => nums.reduce((prev, current) => prev + current, 0);
+console.log(restParams(1, 2, 3, 4, 5));
 export {};
--- FrontEnd/Typescript/Chapter4_Functions/src/main.ts
+++ FrontEnd/Typescript/Chapter4_Functions/src/main.ts
@@ -5,7 +5,7 @@ type onlyNumber = number;
 
 interface userData {
     name: string,
-    age?: numOrString    
+    age?: numOrString
     address?: string,
     hobbies?: stringOrNumber
 }
@@ -27,11 +27,66 @@ const moveDirection = (dir: "East" | "West" | "North" | "South") => `Move ${dir}
 console.log(moveDirection("West"));
 
 // Functions :- with Return
-const add = (a:onlyNumber, b:onlyNumber): onlyNumber => a + b;
-console.log(add(2,3))
+const add = (a: onlyNumber, b: onlyNumber): onlyNumber => a + b;
+console.log(add(2, 3))
 
 // Function without return (void) :-
-const greetUser = (user:userData): void => {
+const greetUser = (user: userData): void => {
     console.log(user.name);
 }
-greetUser({name:"Sudalaimani"})
+greetUser({ name: "Sudalaimani" })
+
+//Anonymous Function :-
+const subtract = function (c: onlyNumber, d: onlyNumber): onlyNumber {
+    return c + d
+}
+console.log(subtract(16, 5));
+
+// We can also make the type for the similar parameter process function
+type calcualtion = (a: onlyNumber, b: onlyNumber) => onlyNumber
+
+const multiplication: calcualtion = (a, b) => a * b
+console.log(multiplication(2, 4))
+
+const division: calcualtion = (a, b) => a / b;
+console.log(division(6, 2));
+
+// In Anonymous Function
+let mul: calcualtion = function (a, b): number {
+    return a * b
+}
+console.log(mul(6, 2));
+
+//We can also do this in Interface :-
+interface cal {
+    (a: onlyNumber, b: onlyNumber): onlyNumber
+}
+
+const addition: cal = (a, b): number => a + b;
+console.log(addition(4, 5));
+
+// Anonymous Func :-
+const additionFunc: cal = function (a, b): number {
+    return a + b;
+}
+console.log(additionFunc(30, 24));
+
+
+// Optional Parmeter :- We use ? in the func params to define the params as optional and it is defined as undefined
+const addAll = (a: number, b: number, c?: number): number => { //We must use the optional params as the last params in the function
+    if (typeof c === "undefined") {
+        return a + b
+    }
+    return a + b + c
+}
+
+// Default Params :-
+const sumAll = (a: number, b: number, c: number = 4): number => a + b + c;
+console.log(sumAll(3, 4)); //If the default params is in the first or the other places except the last, we must pass undefined when we invoke the function
+
+const minusAll = (a: number = 3, b: number = 4, c: number): number => a + b + c
+console.log(minusAll(undefined, undefined, 10)); //Because without we use undefined the number 10 is assigned to the first or the respective params.
+
+// Rest Params (Operator):-
+const restParams = (...nums: number[]): number => nums.reduce((prev, current) => prev + current, 0);
+console.log(restParams(1, 2, 3, 4, 5));
