361ce97221ac88a16f92634e3747133b09120e23
K.Sudalaimani <sudalaimani5112007@gmail.com>
2026-08-04 10:43:15 +0530

    Updated the syntax of the update-controller

--- Tasks/Backend/Express/Day6/server/src/controller/update-task.controller.ts
+++ Tasks/Backend/Express/Day6/server/src/controller/update-task.controller.ts
@@ -4,30 +4,37 @@ import Task_Model from "../model/task.model.js";
 const updateTaskController = async (request: Request, response: Response): Promise<void> => {
     try {
         const { id } = request.params;
-        const {task, taskDescription} = request.body;
+        const { task, taskDescription } = request.body;
+
+        if (!task || !taskDescription) {
+            response.status(409).json({
+                "message": "Enter the valid task and task description"
+            })
+            return;
+        }
 
         const updateTask = await Task_Model.findByIdAndUpdate(
             id,
-            {task, taskDescription},
-            {new:true, runValidators:true}
+            { task, taskDescription },
+            { new: true, runValidators: true }
         );
 
-        if(!updateTask){
+        if (!updateTask) {
             response.status(404).json({
-                "message":"Task not found to update"
+                "message": "Task not found to update"
             })
             return;
         }
 
         response.status(200).json({
-            "message":"Task updated successfully",
-            "updated_task":updateTask
+            "message": "Task updated successfully",
+            "updated_task": updateTask
         })
 
-    } catch (error:any) {   
+    } catch (error: any) {
         response.status(500).json({
-            "message":"Something went wrong while updating the task",
-            "error_message":error.message
+            "message": "Something went wrong while updating the task",
+            "error_message": error.message
         })
     }
 }
