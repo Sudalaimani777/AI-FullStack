d071c4d0a90566ee0988e844554698c7759b4bea
K.Sudalaimani <sudalaimani5112007@gmail.com>
2026-08-23 11:33:38 +0530

    Add detailed notes on Client and Server Components in Next.js

---
+++ FrontEnd/Next/1-nextjs-essentials/3-cient-and-server-component-understanding/components/ClientButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ClientButton = () => {
+    return (
+        <>
+            <button onClick={() => console.log("Client button clicked")}>Client Button</button>
+        </>
+    )
+}
+
+export default ClientButton
